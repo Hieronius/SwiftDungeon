ecde906eba3ed6c5092ae798252dad927de0c890
Arsenty Halimovsky <ghost12@yandex.ru>
2025-04-30 09:47:56 +0300

    Add comments

--- SwiftDungeon/SwiftDungeon/Managers/CombatManager.swift
+++ SwiftDungeon/SwiftDungeon/Managers/CombatManager.swift
@@ -2,16 +2,17 @@ import Foundation
 
 class CombatManager {
 
+	/// Calculates average from host min/max damage and subtracting target's current armor
 	func attack(_ host: Character, _ target: Character) -> Int {
 		Int.random(in: host.minDamage...host.maxDamage) - target.currentArmor
 	}
 
-	/// Adds armor value
+	/// Adds blockValue to caster currentArmor for one turn
 	func block(_ caster: Character) -> Int {
 		caster.currentArmor + caster.blockValue
 	}
 
-	// should be refactored so the method only calculates heal instead of using game state properties
+	/// Heals host by his spellPower value
 	func heal(_ host: Character) -> Int {
 		host.spellPower
 	}
