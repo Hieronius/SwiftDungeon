0c697098c1e080ed39c4c920dfe72d5e2e6b9edd
Arsenty Halimovsky <ghost12@yandex.ru>
2025-05-03 07:16:24 +0300

    Create GameConfig

--- SwiftDungeon/SwiftDungeon/Domain/GameConfig.swift
+++ SwiftDungeon/SwiftDungeon/Domain/GameConfig.swift
@@ -2,5 +2,22 @@ import Foundation
 
 struct GameConfig {
 
-	
+	// Dungeon Config
+
+	static let dungeonLength = 14
+	static let expPerRoom = 50
+
+	// Abilities
+
+	static let healManaCost = 10
+	static let buffManaCost = 10
+
+	static let attackEnergyCost = 1
+	static let blockEnergyCost = 1
+	static let spellEnergyCost = 1
+	static let cutEnergyCost = 1
+
+	static let blockDuration = 1
+	static let cutDuration = 3
+	static let buffDuration = 1
 }
