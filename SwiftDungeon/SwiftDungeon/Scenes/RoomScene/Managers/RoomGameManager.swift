3c8c5ec26a27de2f1dde695ab1ed26976c2f26ae
Arsenty Halimovsky <ghost12@yandex.ru>
2025-05-14 11:18:47 +0300

    Refactor dot()

--- SwiftDungeon/SwiftDungeon/Scenes/RoomScene/Managers/RoomGameManager.swift
+++ SwiftDungeon/SwiftDungeon/Scenes/RoomScene/Managers/RoomGameManager.swift
@@ -579,13 +579,14 @@ extension RoomGameManager {
 	/// Periodic Magic Damage
 	func dot() {
 
-		guard let snapshot = roomGameState
-			.checkIsGameOneCurrentTurnHeroAndEnemy()
-		else { return }
+		let snapshot = roomGameState.getActualGameStateSnapshot()
 
-		let host = snapshot.host
-		let target = snapshot.target
 		let isHeroTurn = snapshot.isHeroTurn
+		guard let hero = snapshot.hero else { return }
+		guard let enemy = snapshot.enemy else { return }
+
+		let host = isHeroTurn ? hero : enemy
+		let target = isHeroTurn ? enemy : hero
 
 		guard host.currentMana >= GameConfig.dotManaCost,
 			  host.currentEnergy >= GameConfig.spellEnergyCost else {
