ee762c7f6edd52c713055c1193f664cc0d457fda
Arsenty Halimovsky <ghost12@yandex.ru>
2025-05-17 07:58:15 +0300

    Update back

--- SwiftDungeon/SwiftDungeon/Scenes/RoomScene/Model/HeroUIState.swift
+++ SwiftDungeon/SwiftDungeon/Scenes/RoomScene/Model/HeroUIState.swift
@@ -4,7 +4,7 @@ import SwiftUI
 
 /// Snapshot of Hero's current stats and effects
 struct HeroUIState {
-
+	
 	var heroCurrentLevel: Int
 	var heroMaxHealth: Int
 	var heroCurrentHealth: Int
@@ -18,56 +18,5 @@ struct HeroUIState {
 	var heroActionLabel: Int
 	var heroBeingHit: Bool
 	var heroActiveEffects: [Effect]
-
-	init(
-		heroCurrentLevel: Int,
-		heroMaxHealth: Int,
-		heroCurrentHealth: Int,
-		heroMaxMana: Int,
-		heroCurrentMana: Int,
-		heroMaxEnergy: Int,
-		heroCurrentEnergy: Int,
-		heroCurrentExperience: Int,
-		heroMaxExperience: Int,
-		heroActionColor: Color,
-		heroActionLabel: Int,
-		heroBeingHit: Bool,
-		heroActiveEffects: [Effect]
-	) {
-		self.heroCurrentLevel = heroCurrentLevel
-		self.heroMaxHealth = heroMaxHealth
-		self.heroCurrentHealth = heroCurrentHealth
-		self.heroMaxMana = heroMaxMana
-		self.heroCurrentMana = heroCurrentMana
-		self.heroMaxEnergy = heroMaxEnergy
-		self.heroCurrentEnergy = heroCurrentEnergy
-		self.heroCurrentExperience = heroCurrentExperience
-		self.heroMaxExperience = heroMaxExperience
-		self.heroActionColor = heroActionColor
-		self.heroActionLabel = heroActionLabel
-		self.heroBeingHit = heroBeingHit
-		self.heroActiveEffects = heroActiveEffects
-	}
-}
-
-extension HeroUIState {
-
-	init(from snapshot: RoomGameStateSnapshot) {
-
-		guard let hero = snapshot.hero else { return }
-	heroCurrentLevel = hero.stats.level
-	heroMaxHealth = hero.maxHealth
-		heroCurrentHealth = hero.currentHealth
-		heroMaxMana = hero.maxMana
-		heroCurrentMana = hero.currentMana
-		heroMaxEnergy = hero.maxEnergy
-		heroCurrentEnergy = hero.currentEnergy
-		heroCurrentExperience = hero.stats.currentExperience
-		heroMaxExperience = hero.stats.maxExperience
-		heroActionColor = hero.heroUIState.heroActionColor
-		heroActionLabel = snapshot.heroUIState.heroActionLabel
-		heroBeingHit = snapshot.heroUIState.heroBeingHit
-		heroActiveEffects = snapshot.hero.activeEffects
-	}
-
+	
 }
